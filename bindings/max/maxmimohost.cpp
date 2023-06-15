90403f2ec3e31817db42cf332d87ef83016dd6d7
diemo <diemo.schwarz@ircam.fr>
2023-06-15 16:28:20 +0200

    final fix for https://git.forum.ircam.fr/haddad/mubu/-/issues/270: don't prepend module name here

--- bindings/max/maxmimohost.cpp
+++ bindings/max/maxmimohost.cpp
@@ -4,7 +4,7 @@
 Mimo *MaxMimoHost::set_module (const char *str, Mimo *receiver)
 {
 #if USE_PIPO_GRAPH
-  if (chain.create(str)) // does instantiate and wire to connect graph pipos
+  if (chain.create(str, false)) // does instantiate and wire to connect graph pipos
   {
     //return dynamic_cast<Mimo *>(&chain);
     return dynamic_cast<Mimo *>(chain.getPiPo((unsigned int)0));
