9c374a63beb5bec5b05cf5352f7ebf09eefe7a5f
Diemo Schwarz <schwarz@ircam.fr>
2025-02-05 10:20:17 +0100

    silence warning about std::binary_function being deprecated, this feature was pulled in despite the chosen c++ version

--- src/PiPo.h
+++ src/PiPo.h
@@ -935,7 +935,7 @@ public:
    */
   class EnumAttr : public Attr
   {
-#if __cplusplus < 201703L // c++11 or c++14
+#if 0 // __cplusplus < 201703L // c++11 or c++14
     struct strCompare : public std::binary_function<const char *, const char *, bool>
 #else // from c++17 on, std::binary_function is no longer necessary and has been removed
     struct strCompare
